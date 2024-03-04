8af54e9a1c605420f44d8037c282944b0bc9e4ee
ekistrimova <90572032+ekistrimova@users.noreply.github.com>
2024-03-04 16:19:58 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,7 +3,7 @@
 #include <cstdint>
 #include "alg.h"
 
-TEST(Check_Prime, TestSmallNum) {
+TEST(Check_prime, TestSmallNum) {
     EXPECT_FALSE(checkPrime(4));
     EXPECT_FALSE(checkPrime(8));
     EXPECT_FALSE(checkPrime(15));
@@ -12,53 +12,53 @@ TEST(Check_Prime, TestSmallNum) {
     EXPECT_TRUE(checkPrime(13));
 }
 
-TEST(Check_Prime, TestBigNum) {
+TEST(Check_prime, TestBigNum) {
     EXPECT_FALSE(checkPrime(5e8 + 10));
     EXPECT_TRUE(checkPrime(27'644'437));
 }
 
-TEST(Check_Prime, TestOneIsPrime) {
+TEST(Check_prime, TestOneIsPrime) {
     EXPECT_FALSE(checkPrime(1));
 }
 
-TEST(NPrime, TestSmallNum) {
+TEST(N_Prime, TestSmallNum) {
     EXPECT_EQ(nPrime(2), 3);
     EXPECT_EQ(nPrime(4), 7);
     EXPECT_EQ(nPrime(7), 17);
 }
 
-TEST(NPrime, TestAnyNum) {
+TEST(N_Prime, TestAnyNum) {
     EXPECT_EQ(nPrime(500), 3571);
     EXPECT_EQ(nPrime(100000), 1299709);
 }
 
-TEST(NextPrine, TestSmallNum) {
+TEST(NextPrime, TestSmallNum) {
     EXPECT_EQ(nextPrime(8), 11);
     EXPECT_EQ(nextPrime(2), 3);
     EXPECT_EQ(nextPrime(21), 23);
 }
 
-TEST(NextPrine, TestBigNum) {
+TEST(NextPrime, TestBigNum) {
     EXPECT_EQ(nextPrime(1e9 + 7), 1e9 + 9);
     EXPECT_EQ(nextPrime(1e9 + 8), 1e9 + 9);
 }
 
-TEST(SumPrime, TestSmallNum) {
+TEST(SPrime, TestSmallNum) {
     EXPECT_EQ(sumPrime(10), 17);
     EXPECT_EQ(sumPrime(15), 41);
 }
 
-TEST(SumPrime, TestAnyNum) {
+TEST(SPrime, TestAnyNum) {
     EXPECT_EQ(sumPrime(1000), 76127);
     EXPECT_EQ(sumPrime(2875), 552753);
 }
 
-TEST(SumPrime, TestDifSum) {
+TEST(SPrime, TestDifSum) {
     int res1 = sumPrime(20);
     int res2 = sumPrime(25);
     EXPECT_EQ(res2 - res1, 23);
 }
 
-TEST(SumPrime, TestSumLessTwoMil) {
+TEST(SPrime, TestSumLessTwoMil) {
     EXPECT_EQ(sumPrime(2e6), 142913828922);
 }
